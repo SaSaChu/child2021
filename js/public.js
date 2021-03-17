fc995493a249132a95dc2a0ff7cc20fdd18c62cf
SaSaChu <teresa7397@gmail.com>
2021-03-17 22:14:06 +0800

    上完資料

--- js/public.js
+++ js/public.js
@@ -1,2 +1,12 @@
 $(function() {
+
+  $('.m-menuLink').click(function() {
+    $('.m-menuBox').toggleClass('m-menuBox-s');
+  });
+
+  $('.m-drop').click(function() {
+    $('.m-dropboxs').toggleClass('m-dropboxs-s');
+  });
+
+
 });
